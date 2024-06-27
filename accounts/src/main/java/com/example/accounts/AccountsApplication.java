fc8887967accadc369c170fb73028b283295c28a
Inna Lapina <jyliana@gmail.com>
2024-06-27 12:58:33 +0300

    rewriting accounts

--- accounts/src/main/java/com/example/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/example/accounts/AccountsApplication.java
@@ -1,29 +1,13 @@
 package com.example.accounts;
 
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@RefreshScope
-@ComponentScans({@ComponentScan("com.example.accounts.controller")})
-@EnableJpaRepositories("com.example.accounts.repository")
-@EntityScan("com.example.accounts.model")
 public class AccountsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AccountsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
 
-    @Bean
-    public TimedAspect timedAspect(MeterRegistry registry) {
-        return new TimedAspect(registry);
-    }
 }
