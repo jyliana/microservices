41447e5aab45e2efa4a7f20a67c464dc841b68d7
Inna Lapina <jyliana@gmail.com>
2024-06-28 20:57:44 +0300

    continue with REST CRUD

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -4,28 +4,61 @@ import com.example.accounts.constants.AccountsConstants;
 import com.example.accounts.dto.CustomerDto;
 import com.example.accounts.dto.ResponseDto;
 import com.example.accounts.service.IAccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
   private IAccountsService iAccountsService;
 
   @PostMapping("/create")
-  public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+  public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
     iAccountsService.createAccount(customerDto);
 
-    return ResponseEntity
-            .status(HttpStatus.CREATED)
+    return ResponseEntity.status(HttpStatus.CREATED)
             .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
   }
+
+  @GetMapping("/fetch")
+  public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
+                                                         @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                         String mobileNumber) {
+    var customerDto = iAccountsService.fetchAccount(mobileNumber);
+    return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+  }
+
+  @PutMapping("/update")
+  public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+    boolean isUpdated = iAccountsService.updateAccount(customerDto);
+    return isUpdated ?
+            ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200))
+            :
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+  }
+
+  @DeleteMapping("/delete")
+  public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
+                                                          @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                          String mobileNumber) {
+    boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+    return isDeleted ?
+            ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200))
+            :
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+  }
 }
+
--- accounts/src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.accounts.service.impl;
 
 import com.example.accounts.constants.AccountsConstants;
+import com.example.accounts.dto.AccountsDto;
 import com.example.accounts.dto.CustomerDto;
 import com.example.accounts.enitity.Accounts;
 import com.example.accounts.enitity.Customer;
 import com.example.accounts.exception.CustomerAlreadyExistsException;
+import com.example.accounts.exception.ResourceNotFoundException;
+import com.example.accounts.mapper.AccountsMapper;
 import com.example.accounts.mapper.CustomerMapper;
 import com.example.accounts.repository.AccountsRepository;
 import com.example.accounts.repository.CustomerRepository;
@@ -12,7 +15,6 @@ import com.example.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Random;
 
 @Service
@@ -26,13 +28,60 @@ public class AccountsServiceImpl implements IAccountsService {
   public void createAccount(CustomerDto customerDto) {
     var customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
     var optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+
     if (optionalCustomer.isPresent()) {
       throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber " + customerDto.getMobileNumber());
     }
-    customer.setCreatedAt(LocalDateTime.now());
-    customer.setCreatedBy("Anonymous");
+
     var savedCustomer = customerRepository.save(customer);
     accountsRepository.save(createNewAccount(savedCustomer));
+  }
+
+  @Override
+  public CustomerDto fetchAccount(String mobileNumber) {
+    var customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+            () -> new ResourceNotFoundException("Customer", "mobile number", mobileNumber));
+
+    var accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+            () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+    );
+
+    var customerDto = CustomerMapper.mapToCustomerDto(customer, new CustomerDto());
+    customerDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+    return customerDto;
+  }
+
+  @Override
+  public boolean updateAccount(CustomerDto customerDto) {
+    boolean isUpdated = false;
+    var accountsDto = customerDto.getAccountsDto();
+    if (accountsDto != null) {
+      var accounts = accountsRepository.findById(accountsDto.getAccountNumber()).orElseThrow(
+              () -> new ResourceNotFoundException("Account", "AccountNumber", accountsDto.getAccountNumber().toString())
+      );
+      AccountsMapper.mapToAccounts(accountsDto, accounts);
+      accounts = accountsRepository.save(accounts);
+
+      Long customerId = accounts.getCustomerId();
+      Customer customer = customerRepository.findById(customerId).orElseThrow(
+              () -> new ResourceNotFoundException("Customer", "CustomerID", customerId.toString())
+      );
+      CustomerMapper.mapToCustomer(customerDto, customer);
+      customerRepository.save(customer);
+      isUpdated = true;
+    }
+    return isUpdated;
+  }
+
+  @Override
+  public boolean deleteAccount(String mobileNumber) {
+    var customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+            () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+    );
+    accountsRepository.deleteByCustomerId(customer.getCustomerId());
+    customerRepository.deleteById(customer.getCustomerId());
+    return true;
   }
 
   /**
@@ -47,8 +96,6 @@ public class AccountsServiceImpl implements IAccountsService {
     newAccount.setAccountNumber(randomAccNumber);
     newAccount.setAccountType(AccountsConstants.SAVINGS);
     newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-    newAccount.setCreatedAt(LocalDateTime.now());
-    newAccount.setCreatedBy("Anonymous");
     return newAccount;
   }
 
